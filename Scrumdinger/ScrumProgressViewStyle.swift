71633de556e42c6425f50520bb364c5f45fc90f4
ImaevDmitrii <121541029+ImaevDmitrii@users.noreply.github.com>
2023-01-17 15:28:28 +0300

    Managing state and life cycle

---
+++ Scrumdinger/ScrumProgressViewStyle.swift
@@ -0,0 +1,34 @@
+//
+//  ScrumProgressViewStyle.swift
+//  Scrumdinger
+//
+//  Created by Дмитрий Имаев on 17.01.2023.
+//
+
+import SwiftUI
+
+struct ScrumProgressViewStyle: ProgressViewStyle {
+    
+    var theme: Theme
+    
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(theme.accentColor)
+                .frame(height: 20.0)
+                ProgressView(configuration)
+                    .tint(theme.mainColor)
+                    .frame(height: 12.0)
+                    .padding(.horizontal)
+        }
+    }
+}
+
+
+struct ScrumProgressViewStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressView(value: 0.4)
+            .progressViewStyle(ScrumProgressViewStyle(theme: .buttercup))
+            .previewLayout(.sizeThatFits)
+    }
+}
